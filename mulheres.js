e874053154a403a9afd088d6a5d4f2f98748f7d0
yasmindornelas <yasmindornelaslook@gmail.com>
2023-06-24 15:07:07 -0300

    insere a nova versão do projeto integrado com o mongodb

--- mulheres.js
+++ mulheres.js
@@ -1,34 +1,95 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express") //aqui estou iniciando o express
+const router = express.Router() //aqui estou configurando a primeira parte da rota
+const cors = require('cors') //aqui estou trazendo o pacote cors que permite consumir essa API no front-end
+const conectaBancoDeDados = require('./BancoDeDados') //aqui estou ligando ao arquivo BancoDeDados
+conectaBancoDeDados() //chamando a função que conecta o banco de dados
 
-const app = express()
-const porta =3333
+const Mulher = require('./mulherModel')
 
-    const mulheres =[
-    {
-        nome:'Yasmin Dornelas',
-        imagem: 'https://media.licdn.com/dms/image/C4D03AQFIlJfbvGXCag/profile-displayphoto-shrink_200_200/0/1660761599038?e=1691625600&v=beta&t=gfL0sPdORIAk04AyfAoKtozjDj5E-QaFZh1IMgvmRtE',
-        minibio: 'Desenvolvedora iniciante'
-    },
-    {
-        nome: 'Ceriza Moreira',
-        imagem: 'https://www.facebook.com/photo/?fbid=881762078628505&set=a.105746706230050&__tn__=%3C',
-        minibio: 'Design de interiores'
-    },
-    {
-        nome:'Amanda Dornelas',
-        imagem:'https://scontent.fbsb8-1.fna.fbcdn.net/v/t39.30808-6/273460089_2517317021736453_1943813178652992613_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeG-H9S6JxC-a51HIoidqfzEFoeDJru-g6gWh4Mmu76DqGtj4ug22XuwaetPNUey63_wcm-9URW9jpAvnQm2-B8I&_nc_ohc=i9jwqtWQO9cAX8rlGkV&_nc_ht=scontent.fbsb8-1.fna&oh=00_AfDm2DYd86sVu_BVGYrJDJuLkYNBp3zGxdfb4v5RVq_8uQ&oe=64810490',
-        minibio:'Esteticista e Biomedica'
+const app = express() //aqui estou iniciando o app
+app.use(express.json())
+app.use(cors())
+
+const porta =3333 //aqui estou criando a porta
+
+//GET
+async function mostraMulheres(request, response) {
+    try{
+        const mulheresVindasDoBancoDeDados = await Mulher.find()
+
+        response.json(mulheresVindasDoBancoDeDados)
+    }catch (erro){
+        console.log(erro)
     }
-]
-
-function mostraMulheres(request, response) {
-    response.json(mulheres)
 }
 
+//POST
+async function criaMulher(request, response) {
+    const novaMulher = new Mulher({
+        nome: request.body.nome,
+        imagem: request.body.imagem,
+        minibio: request.body.minibio,
+        citação: request.body.citação
+    })
+
+    try {
+        const mulherCriada = await novaMulher.save()
+        response.status(201).json(mulherCriada)
+    } catch (erro) {
+        console.log(erro)
+    }   
+}
+   
+//PATCH
+async function corrigeMulher(request, response) {
+    try {
+        const mulherEncontrada = await Mulher.findById(request.params.id)
+        
+        if (request.body.nome) {
+            mulherEcontrada.nome = request.body.nome
+        }
+    
+        if (request.body.minibio) {
+            mulherEcontrada.minibio = request.body.minibio
+        }
+    
+        if (request.body.imagem) {
+            mulherEcontrada =request.body.imagem
+        }
+
+        if (request.body.citação) {
+            mulherEncontrada = request.body.citação
+        }
+
+        const mulherAtualizadaNoBancoDeDados = await mulherEncontrada.save()
+        
+        response.json(mulherAtualizadaNoBancoDeDados) 
+
+    } catch (erro) {
+        console.log(erro)
+    }
+    
+}
+//Delete
+async function deleteMulher(request, response) {
+    try {
+        await Mulher.findByIdAndDelete(request.params.id)
+        response.json({mensagem: 'Mulher deletada com sucesso!'})
+    } catch(erro) {
+        console.log(erro)
+    }
+}
+
+
+app.use(router.get('/mulheres', mostraMulheres)) //configurei rota GET /mulheres
+app.use(router.post('/mulheres',criaMulher)) //configurei rota POST /mulheres
+app.use(router.patch('/mulheres/:id', corrigeMulher)) //configurei rota PATCH /mulheres/:id
+app.use(router.delete('/mulheres/:id', deleteMulher)) //configurei rota DELETE /mulheres/:id
+
+//PORTA
 function mostraPorta() {
     console.log("Servidor criado e rodando na porta ", porta)
 }
 
-app.use(router.get('/mulheres', mostraMulheres))
-app.listen(porta, mostraPorta)
+
+app.listen(porta, mostraPorta) //servidor ouvindo a porta
